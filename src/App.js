bc14b2ddd34ab51c13e99244e3f81b7c630fb493
k5953837 <k5953837@gmail.com>
2022-01-24 14:41:21 +0800

    Add tailwind ui list style.
    Signed-off-by: k5953837 <k5953837@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import JobsList from './components/JobsList';
 function App() {
 
   return (
-    <div className="App">
-      <h1>Jobs</h1>
-      <JobsList />
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Jobs</h2>
+          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Find your dream job.
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            Start your search for a job today.
+          </p>
+        </div>
+        <JobsList />
+      </div>
     </div>
   );
 }
--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { CalendarIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
 const JobsList = props => {
 
@@ -11,15 +12,34 @@ const JobsList = props => {
   const [jobs, setJobs] = useState([]);
 
   return (
-      <div>
-        <div className="jobs-list">
-          {jobs.map((job, index) => (
-              <div key={index}>
-                {job.company} | {job.position} | {job.description}
+    <div className="bg-white shadow overflow-hidden sm:rounded-md mt-10">
+      <ul role="list" className="divide-y divide-gray-200">
+        {jobs.map((job) => (
+          <li key={job.id}>
+            <a href="#" className="block hover:bg-gray-50">
+              <div className="px-4 py-4 flex items-center sm:px-6">
+                <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                  <div className="truncate">
+                    <div className="flex text-sm">
+                      <p className="font-medium text-indigo-600 truncate">{job.company}</p>
+                      <p className="ml-1 flex-shrink-0 font-normal text-gray-500">in {job.position}</p>
+                    </div>
+                    <div className="mt-2 flex">
+                      <div className="flex items-center text-sm text-gray-500">
+                        <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                        <p>
+                          Starting on <time dateTime={job.created_at}>{job.updated_at}</time>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            ))}
-        </div>
-      </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 };
 export default JobsList;
